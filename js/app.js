0d4f7be5ccf0e7519a43ed67c9822dfcc011e692
Ya Prantik <yaprantik@gmail.com>
2021-11-07 00:33:47 +0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,84 @@
+
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click', function (e) {
+    let addTxt = document.getElementById('addTxt');
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+        
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    addTxt.value = '';
+    showNotes()
+    
+})
+
+function showNotes() {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+        
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    html = '';
+    notesObj.forEach(function (element, index) {
+        html += `
+        <div class=" noteCard my-3 mx-2 card" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">Note ${index + 1}</h5>
+        <p class="card-text">${element}</p>
+        <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+        </div>
+        </div>
+        
+        `;
+        
+    });
+    let notesElm = document.getElementById('notes')
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html
+    }
+    else {
+        notesElm.innerHTML = `Nothing to show! Use "Add Note" section above to add notes.`
+    }
+}
+
+function deleteNote(index) {
+    let notes = localStorage.getItem('notes');
+    
+    if (notes == null) {
+        notesObj = [];
+        
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    showNotes()
+}
+
+let search = document.getElementById('searchTxt');
+search.addEventListener('input', function () {
+    let inpVal = search.value.toLowerCase();
+    let noteCard = document.getElementsByClassName('noteCard');
+    Array.from(noteCard).forEach(function(element){
+        let cardTst = element.getElementsByTagName('p')[0].innerText;
+        if(cardTst.includes(inpVal)){
+            element.style.display = 'block';
+            
+        }
+        else{
+            element.style.display = 'none';
+            
+        }
+    })
+    
+})
+showNotes()
